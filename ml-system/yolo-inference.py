7da342a49da464b12389073026a3a5892039cda1
Derrick Ampire <ampire90@gmail.com>
2024-04-25 15:36:05 +0300

    update: used the trained model

--- ml-system/yolo-inference.py
+++ ml-system/yolo-inference.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # model = YOLO('yolov8x')
-model = YOLO('training/runs/detect/train3/weights/last.pt')
+model = YOLO('trained-models/yolo5_last.pt')
 
 res = model.predict('input-data/input_video.mp4', conf=0.2, save=True)
 
