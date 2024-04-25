9552b3e321b21ee7729d78cff2f61815dede06ca
Derrick Ampire <ampire90@gmail.com>
2024-04-25 15:48:23 +0300

    feat: tracking added by id

--- ml-system/yolo-inference.py
+++ ml-system/yolo-inference.py
@@ -1,11 +1,13 @@
 from ultralytics import YOLO
 
-# model = YOLO('yolov8x')
-model = YOLO('trained-models/yolo5_last.pt')
+model = YOLO('yolov8x')
+# model = YOLO('trained-models/yolo5_last.pt')
 
-res = model.predict('input-data/input_video.mp4', conf=0.2, save=True)
+res = model.track('input-data/input_video.mp4', conf=0.2, save=True)
 
+"""
 print(res)
 print("boxes:")
 for box in res[0].boxes:
     print(box)
+"""
